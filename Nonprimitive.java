134b3bdab3f515540d33a6385e4d2de126f7c2d0
UfwMahesh17 <maheshwaran.2023ads@kveg.in>
2025-02-05 20:58:51 +0530

    Create Nonprimitive.java

---
+++ Nonprimitive.java
@@ -0,0 +1,25 @@
+public class Nonprimitive {
+    
+    public static void main (String [] args ){
+        
+        boolean condition = true;
+        int num = 12 ;
+        char variable = 'M';
+        short s = 12 ;
+        byte b = 1;
+        long l = 1234567;
+        float f = 3.14676767f;
+        double d = 3.14676767;
+        
+        System.out.println("Boolean Condition : "+condition);
+        System.out.println("Integer Value : "+num);
+        System.out.println("Char value : "+variable);
+        System.out.println("Short Value : "+s);
+        System.out.println("IByte Value : "+b);
+        System.out.println("Long Value : "+l);
+        System.out.println("Float Value : "+f);
+        System.out.println("Double Value : "+d);
+        
+    }
+    
+}
